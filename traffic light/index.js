539ae1fb12ac63ada59f564b4e92b652b8a1eb33
anshuvais <139906375+anshuvais@users.noreply.github.com>
2023-07-29 15:45:03 +0530

    Add files via upload

---
+++ traffic light/index.js
@@ -0,0 +1,39 @@
+function redOff(){
+  
+  
+    var obj =document.getElementsByClassName('c1')
+    obj[0].style.backgroundColor = "#fff"
+   // var obj =  document.getElementByClass("c1").style.
+   //     background= "#fff";
+   }
+     function greenOff(){
+     
+     document.getElementById("c12").style.
+       background= "#fff";
+     }
+   function yellowOff(){
+     
+     document.getElementById("c13").style.
+       background= "#fff";
+   }
+   function lightOff(){
+     
+     document.getElementById("c12").style.
+       background= "#fff";
+     
+   }
+   function pressRed(){
+    
+     var obj =document.getElementsByClassName('c1')
+    obj[0].style.backgroundColor = "#ff0000";
+   }
+   function pressGreen(){
+     
+     document.getElementById("c12").style.
+       background= "#00ff00";
+   }
+   function pressYellow(){
+   
+     document.getElementById("c13").style.
+       background= "#faf605";
+   }
